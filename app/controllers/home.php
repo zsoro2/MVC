71a57c8637bb13fc67651a4879ab33b39654f59e
jolvanzsoltom <zsoro2@hotmail.com>
2014-07-13 10:54:44 +0200

    Mvc
    mvc 2

---
+++ app/controllers/home.php
@@ -0,0 +1,9 @@
+<?php
+
+class Home extends Controller{
+	public function index($name=''){
+		$user = $this->model('User');
+		$user->name = $name;
+		$this->view('home/index',['name' => $user->name]);
+	}
+}
